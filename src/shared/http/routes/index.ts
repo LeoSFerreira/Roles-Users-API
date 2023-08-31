f1b41014a34679a023bff97e78a7e6f7a62749c9
unknown <contato.leosf@gmail.com>
2023-08-31 15:29:09 -0300

    Add errors middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
+import { AppError } from 'src/shared/errors/AppError'
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
+  throw new AppError('Acesso negado')
   return response.json({ message: 'Olá, mundo!' })
 })
 
