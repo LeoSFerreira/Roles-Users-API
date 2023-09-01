0211408c169fca36d70f57ba20aa96b6b4c1cecc
unknown <contato.leosf@gmail.com>
2023-09-01 13:57:19 -0300

    Add role findAll

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express'
 import { RolesRepository } from 'src/roles/repositories/RoleRepository'
 const rolesRouter = Router()
+const rolesRepository = new RolesRepository()
 
 rolesRouter.post('/', (request, response) => {
   const { name } = request.body
-  const rolesRepository = new RolesRepository()
 
   const role = rolesRepository.create({ name })
 
   return response.status(201).json(role)
+})
+
+rolesRouter.get('/', (request, response) => {
+  const roles = rolesRepository.findAll()
+  return response.json(roles)
 })
 
 export { rolesRouter }
