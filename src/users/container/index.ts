e6ec6c1e68a4416eecfb25065e74e3cff10281bf
unknown <contato.leosf@gmail.com>
2023-09-06 15:30:58 -0300

    Create RefreshToken with createLogin useCase

--- src/users/container/index.ts
+++ src/users/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe'
+import { IRefreshTokenRepository } from '../repositories/IRefreshTokenRepository'
 import { IUsersRepository } from '../repositories/IUsersRepository'
+import { RefreshTokenRepository } from '../repositories/RefreshTokenRepository'
 import { UsersRepository } from '../repositories/UsersRepository'
 import { CreateLoginController } from '../useCases/createLogin/CreateLoginController'
 import { CreateUserController } from '../useCases/createUser/CreateUserController'
@@ -8,6 +10,11 @@ import { ListUsersController } from '../useCases/listUsers/ListUsersController'
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+)
+
+container.registerSingleton<IRefreshTokenRepository>(
+  'RefreshTokenRepository',
+  RefreshTokenRepository,
 )
 
 container.registerSingleton('CreateUserController', CreateUserController)
